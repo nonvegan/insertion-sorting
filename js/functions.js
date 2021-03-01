2c0481416e585b1ca8a786b47141cab46c93ef10
Pedro Magalhães <49410762+nonvegan@users.noreply.github.com>
2021-03-01 18:03:17 +0000

    Update functions.js

--- js/functions.js
+++ js/functions.js
@@ -7,10 +7,6 @@
      }
  }
 
- function getRandomFloat(min, max) {
-     return Math.random() * (max - min) + min;
- }
-
  function getRandomInt(min, max) {
      return Math.floor(Math.random() * (max - min + 1) + min);
  }
@@ -19,23 +15,15 @@
      return 1000 / fps
  }
 
- function getMousePosElem(click) {
-     return new Vector(click.clientX - click.target.getBoundingClientRect().x, click.clientY - click.target.getBoundingClientRect().y)
- }
+ 
 
- function randomHexColor() {
-     return '#' + (function co(lor) {
-         return (lor += [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'a', 'b', 'c', 'd', 'e', 'f'][Math.floor(Math.random() * 16)]) &&
-             (lor.length == 6) ? lor : co(lor);
-     })('');
- }
 
- function componentToHex(c) {
-     var hex = c.toString(16);
-     return hex.length == 1 ? "0" + hex : hex;
- }
 
  function rgbToHex(r, g, b) {
+   function componentToHex(c) {
+      var hex = c.toString(16);
+      return hex.length == 1 ? "0" + hex : hex;
+  }
      return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
  }
 
@@ -121,13 +109,6 @@
 
  }
 
- function restrain(val, min, max) {
-     if (val > max)
-         return max
-     if (val < min)
-         return min
-     return val
- }
 
  function swap(a, b, array) {
      let tempA = array[a]
